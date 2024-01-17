f1dd63bed7b43665dd533327fc5cf5fe618351b7
jirun <jirun0113@naver.com>
2024-01-17 13:36:55 +0900

    Spring Security 환경설정

---
+++ src/main/java/com/example/healthy/config/MybatisConfig.java
@@ -0,0 +1,38 @@
+package com.example.healthy.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan
+public class MybatisConfig {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:**/mapper/*.xml"));
+        factoryBean.setConfigLocation(new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml"));
+
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory){
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    //트랜잭션을 관리하기 위해 DataSourceTransactionManager을 빈으로 등록
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+}
--- src/main/java/com/example/healthy/config/SpringSecurityConfig.java
+++ src/main/java/com/example/healthy/config/SpringSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -14,10 +15,12 @@ import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity //모든 요청 url이 스프링 시큐리티의 제어를 받도록하는 어노테이션
 public class SpringSecurityConfig {
-
-    //private final UserDetailsService userService;
+    @Bean
+    public BCryptPasswordEncoder encodePassword(){
+        return new BCryptPasswordEncoder();
+    }
 
     //Security Filter Chain : Spring Security에서 제공하는 인증, 인가를 위한 필터들의 모음
     //=> 최신 버전의 Spring Security에서 WebConfigureAdapter가 Deprecated 되고, SecurityFilterChain을 이용하도록 권장
@@ -27,7 +30,7 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors
+                .cors(cors -> cors //기존에 CorsConfigurationSource를 빈으로 등록하여 CORS 설정
                         .configurationSource(request -> {
                             CorsConfiguration configuration = new CorsConfiguration();
                             configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
